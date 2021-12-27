ad72ed281df1d384f650c19787ecd09603ad02d0
Fatih Cüre <curefat@gmail.com>
2021-12-27 21:53:35 +0300

    assignment 3 : cover promise needs

--- src/app.js
+++ src/app.js
@@ -6,19 +6,32 @@ axios
   .then((response) => {
     // Firstly, log response to the console,
     // inspect the response and see that it has data field
-
+    console.log(response);
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
-    const products = null;
+    const { data: products } = response;
 
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
+    products.forEach((product) => {
+      console.log(product.name);
+    });
 
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+
+    const mappedProducts = products
+      .filter((product) => {
+        return product.name.includes("Şal");
+      })
+      .map((product) => {
+        return {
+          image: product.image,
+          name: product.name,
+        };
+      });
 
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
@@ -26,4 +39,13 @@ axios
     // You need to use flexbox
     // Position of image and text is up to you
     // You can use any style you wish
+    const root = document.querySelector("#products");
+    mappedProducts.forEach((product) => {
+      root.innerHTML += `
+      <div class="product">
+        <img src="${product.image}" alt="${product.name}">
+        <p>${product.name}</p>
+      </div>
+      `;
+    });
   });
